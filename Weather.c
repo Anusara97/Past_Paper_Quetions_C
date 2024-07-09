be1d4f901bbe272cf06909156750486fa5eb2914
Anusara97 <anusara777rcm@gmail.com>
2024-07-09 19:36:34 +0530

    Project Modified (1)

--- Weather.c
+++ Weather.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+const NO_OF_REAGIONS = 50;
+
+typedef struct WeatherDataRecord {
+    int centerID;
+    float tempurature;
+    float humidity;
+    int rainfall;
+    float windSpeed;
+}WDR;
+
+typedef struct summeryWeatherReoprt {
+    float maxTempurature;
+    float minHumidity;
+    int totalRainfall;
+    float avgWindSpeed;
+}SWR;
+
+WDR readData();
+void printWeatherReport(WDR Record [] );
+void printSummeryReport(SWR Report);
+float calMaxTempurature(WDR Record []);
+float calMinHumidity(WDR Record []);
+int calTotalRainfall(WDR Record []);
+float calAvgWindSpeed(WDR Record []);
+void displayMenu();
+
+int main() {
+    printf("\t\tMain Menu\n");
+    printf("===========================\n");
+    printf("(1) Add New Record\n");
+    printf("(2) Print Daily Weather Report\n");
+    printf("(3) Print Summery Report\n");
+    return 0;
+}
