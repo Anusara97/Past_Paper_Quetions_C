e94ab13d5be8a9acd7d412c34a62b7b612bb83be
Anusara97 <anusara777rcm@gmail.com>
2024-07-10 11:29:58 +0530

    Project Modified (3)

--- Weather.c
+++ Weather.c
@@ -4,8 +4,11 @@
 
 const NO_OF_REAGIONS = 50;
 
+enum mainMenuOptions {ADD_NEW_RECORD =1, PRINT_WEATHER_REPORT, PRINT_SUMMERY, EXIT};
+
 typedef struct WeatherDataRecord {
     int centerID;
+    char date[15];
     float tempurature;
     float humidity;
     int rainfall;
@@ -20,25 +23,97 @@ typedef struct summeryWeatherReoprt {
 }SWR;
 
 WDR readData();
-void printWeatherReport(WDR Record [] );
+void printWeatherReport(WDR Record [NO_OF_REAGIONS] );
 void printSummeryReport(SWR Report);
-float calMaxTempurature(WDR Record []);
-float calMinHumidity(WDR Record []);
-int calTotalRainfall(WDR Record []);
-float calAvgWindSpeed(WDR Record []);
+float calMaxTempurature(WDR Record [NO_OF_REAGIONS]);
+float calMinHumidity(WDR Record [NO_OF_REAGIONS]);
+int calTotalRainfall(WDR Record [NO_OF_REAGIONS]);
+float calAvgWindSpeed(WDR Record [NO_OF_REAGIONS]);
 
 
 int main() {
-    displayMenu();
+    mainMenuHandler();
     return 0;
 }
 
-void displayMenu() {
+int displayMenu() {
+    int choice;
     printf("\t\tMain Menu\n");
-    printf("===========================\n");
+    printf("=======================================\n");
     printf("(1) Add New Record\n");
     printf("(2) Print Daily Weather Report\n");
-    printf("(3) Print Summery Report\n\n");
+    printf("(3) Print Summery Report\n");
+    printf("(4) Exit\n\n");
 
-    printf("Input any number among 1 - 3: ");
+    printf("Input any number among 1 - 4: ");
+    scanf("%d", &choice);
+    printf("\n");
+
+    return choice;
+}
+
+void mainMenuHandler() {
+    int choice;
+    WDR newRecord;
+    WDR Records [NO_OF_REAGIONS];
+
+    do {
+        choice = displayMenu();
+        switch (choice) {
+            case ADD_NEW_RECORD:
+                newRecord = readData();
+                Records[newRecord.centerID - 1] =newRecord;
+                break;
+            case PRINT_WEATHER_REPORT:
+                printWeatherReport(Records);
+                break;
+            case PRINT_SUMMERY:
+                //printSummeryReport();
+                break;
+            case EXIT:
+                printf("Exit\n");
+                break;
+        }
+    }while (choice!= EXIT);
+}
+
+WDR readData(){
+    int centerID;
+    char date [15];
+    float temp;
+    float humi;
+    int rain;
+    float wSpeed;
+
+    printf("Enter center ID\t\t\t:");
+    scanf("%d", &centerID);
+
+    printf("Enter date (yyyy/mm/dd)\t\t:");
+    scanf(" %[^\n]s", &date);
+
+    printf("Enter Temperature(C)\t\t:");
+    scanf(" %f", &temp);
+
+    printf("Enter Humidity(\%)\t\t:");
+    scanf(" %f", &humi);
+
+    printf("Enter Rainfall(ml)\t\t:");
+    scanf(" %d", &rain);
+
+    printf("Enter Wind Speed(km/h)\t\t:");
+    scanf(" %f", &wSpeed);
+    printf("\n");
+
+    WDR newRecord = {centerID, date, temp, humi, rain,wSpeed};
+
+    return newRecord;
+}
+
+void printWeatherReport(WDR Records [NO_OF_REAGIONS] ) {
+    printf("\t\t Daily Weather Report \(%s\)\n", Records[0].date);
+    printf("CenterID\tTemperature(C)\tHumidity(%)\tRainfall(ml)\tWind Speed(km/h)\n");
+    printf("=====================================================================\n\n");
+    for (int i=0; i<1; i++) {
+        printf("%d\t%.2f\t%.2f\t%d\t%.2f\n\n", Records[i].centerID, Records[i].tempurature, Records[i].humidity, Records[i].rainfall, Records[i].windSpeed);
+    }
 }
